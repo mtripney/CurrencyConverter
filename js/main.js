aa56c4539d85bc8a4b5cc8a6590355dc2a74556e
Mark Tripney <mark.tripney@icloud.com>
2022-10-17 20:26:09 +0100

    Move 'currencies' to own module.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { currencies } from './currencies';
 import { requestOptions } from './headers';
 
 const form = document.querySelector('.main-form');
@@ -6,41 +7,6 @@ const fromSelect = document.querySelector('[name="from-currency"]');
 const toSelect = document.querySelector('[name="to-currency"]');
 const toAmount = document.querySelector('.to-amount');
 const ratesByBase = {};
-
-const currencies = {
-  USD: 'United States Dollar',
-  AUD: 'Australian Dollar',
-  BRL: 'Brazilian Real',
-  GBP: 'British Pound Sterling',
-  BGN: 'Bulgarian Lev',
-  CAD: 'Canadian Dollar',
-  CNY: 'Chinese Yuan',
-  HRK: 'Croatian Kuna',
-  CZK: 'Czech Republic Koruna',
-  DKK: 'Danish Krone',
-  EUR: 'Euro',
-  HKD: 'Hong Kong Dollar',
-  HUF: 'Hungarian Forint',
-  INR: 'Indian Rupee',
-  IDR: 'Indonesian Rupiah',
-  ILS: 'Israeli New Sheqel',
-  JPY: 'Japanese Yen',
-  MYR: 'Malaysian Ringgit',
-  MXN: 'Mexican Peso',
-  NZD: 'New Zealand Dollar',
-  NOK: 'Norwegian Krone',
-  PHP: 'Philippine Peso',
-  PLN: 'Polish Zloty',
-  RON: 'Romanian Leu',
-  RUB: 'Russian Ruble',
-  SGD: 'Singapore Dollar',
-  ZAR: 'South African Rand',
-  KRW: 'South Korean Won',
-  SEK: 'Swedish Krona',
-  CHF: 'Swiss Franc',
-  THB: 'Thai Baht',
-  TRY: 'Turkish Lira',
-};
 
 const fetchRates = async (from) => {
   const res = await fetch(
