5028a2fcd03ed26cd4b835a3fa53526730ab98ff
Mark Tripney <mark.tripney@icloud.com>
2022-10-17 14:41:33 +0100

    Populate dropdowns.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,48 @@
+const fromSelect = document.querySelector('[name="from_currency"]');
+const toSelect = document.querySelector('[name="to_currency"]');
+
 const currencies = {
   USD: 'United States Dollar',
   AUD: 'Australian Dollar',
-  BGN: 'Bulgarian Lev',
   BRL: 'Brazilian Real',
+  GBP: 'British Pound Sterling',
+  BGN: 'Bulgarian Lev',
   CAD: 'Canadian Dollar',
-  CHF: 'Swiss Franc',
   CNY: 'Chinese Yuan',
+  HRK: 'Croatian Kuna',
   CZK: 'Czech Republic Koruna',
   DKK: 'Danish Krone',
-  GBP: 'British Pound Sterling',
+  EUR: 'Euro',
   HKD: 'Hong Kong Dollar',
-  HRK: 'Croatian Kuna',
   HUF: 'Hungarian Forint',
+  INR: 'Indian Rupee',
   IDR: 'Indonesian Rupiah',
   ILS: 'Israeli New Sheqel',
-  INR: 'Indian Rupee',
   JPY: 'Japanese Yen',
-  KRW: 'South Korean Won',
-  MXN: 'Mexican Peso',
   MYR: 'Malaysian Ringgit',
-  NOK: 'Norwegian Krone',
+  MXN: 'Mexican Peso',
   NZD: 'New Zealand Dollar',
+  NOK: 'Norwegian Krone',
   PHP: 'Philippine Peso',
   PLN: 'Polish Zloty',
   RON: 'Romanian Leu',
   RUB: 'Russian Ruble',
-  SEK: 'Swedish Krona',
   SGD: 'Singapore Dollar',
+  ZAR: 'South African Rand',
+  KRW: 'South Korean Won',
+  SEK: 'Swedish Krona',
+  CHF: 'Swiss Franc',
   THB: 'Thai Baht',
   TRY: 'Turkish Lira',
-  ZAR: 'South African Rand',
-  EUR: 'Euro',
 };
+
+const generateOptions = (options) =>
+  Object.entries(options)
+    .map(([code, name]) => `<option value="${code}">${name}</option>`)
+    .join('');
+
+const optionsHTML = generateOptions(currencies);
+
+// Populate the options elements
+fromSelect.innerHTML = optionsHTML;
+toSelect.innerHTML = optionsHTML;
